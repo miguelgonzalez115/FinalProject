3a852f150fd047ae5c8ad106ca27c5b5ff7e10af
Miguel Gonzalez <miguel.gonzalez@students.snow.edu>
2023-04-23 18:37:08 -0600

    Class Inventory

--- Program.cs
+++ Program.cs
@@ -30,5 +30,68 @@ class Item
     }
 }
 
+class Inventory
+{
+    private List<Item> items = new List<Item>();
+
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+    }
+
+    public void EditItem(Item item, string newName, int newQuantity, double newPrice)
+    {
+        item.Name = newName;
+        item.Quantity = newQuantity;
+        item.Price = newPrice;
+    }
+
+    public void Display()
+    {
+        foreach (var item in items)
+        {
+            item.Display();
+        }
+    }
+
+    public List<Item> GetItems()
+    {
+        return items;
+    }
+
+    public void Save(string fileName)
+    {
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            foreach (var item in items)
+            {
+                writer.WriteLine($"{item.Name},{item.Quantity},{item.Price}");
+            }
+        }
+    }
+
+    public void Load(string fileName)
+    {
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] parts = line.Split(',');
+                string name = parts[0];
+                int quantity = int.Parse(parts[1]);
+                double price = double.Parse(parts[2]);
+                Item item = new Item(name, quantity, price);
+                AddItem(item);
+            }
+        }
+    }
+}
+
 
 					
