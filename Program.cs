7e9270624f246937129f0ce7a5a79c84f984fc84
Miguel Gonzalez <miguel.gonzalez@students.snow.edu>
2023-04-23 18:32:25 -0600

    Class Item

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+MIGUEL GONZALEZ
+April - 2023
+Final Project for  PROGRAMMING FUNDAMENTALS (CS-1400-001)
+    "Inventory system"
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Item
+{
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+
+    public Item(string name, int quantity, double price)
+    {
+        Name = name;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}\nQuantity: {Quantity}\nPrice: ${Price}\nTotal Value: ${Quantity * Price}\n");
+    }
+}
+
+
+					
